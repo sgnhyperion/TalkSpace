2dcb56cee7d84cd93c782f3c8f5d46b19f71cd5a
Harsh <hk755063@gmail.com>
2024-12-08 02:56:01 +0530

    authentication implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
+
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, ()=>{
-    console.log("server is running on port 5000");  
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`); 
+    connectDB(); 
 })
