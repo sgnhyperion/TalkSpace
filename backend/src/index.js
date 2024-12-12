8dde4e86a0b4b868495251f1d20cc1d2b309154f
Harsh <hk755063@gmail.com>
2024-12-12 21:59:01 +0530

    login and signup pages added

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 const PORT = process.env.PORT
 
