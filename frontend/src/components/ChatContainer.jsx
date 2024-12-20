ae5ffe4d840025d37bad154e33467ddc971e40cd
Harsh <hk755063@gmail.com>
2024-12-20 23:22:58 +0530

    minor change

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -24,7 +24,7 @@ const ChatContainer = () => {
   return (
     <div className='flex flex-1 flex-col overflow-auto'>
       <ChatHeader />
-      <p>Messages...</p>
+      <p>Messages History</p>
       <MessageInput />
     </div>
   )
