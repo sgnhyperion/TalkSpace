95782b7ab5cbedcbec30847b671603eb2d2cc7d2
Harsh <hk755063@gmail.com>
2024-12-26 05:54:44 +0530

    bug fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,14 +25,13 @@ const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    
-
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-} 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`); 
