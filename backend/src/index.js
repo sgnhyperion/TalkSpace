11c5e2befcff91acef4932ccf25ccebc4f3bdb39
Harsh <hk755063@gmail.com>
2024-12-26 05:38:56 +0530

    minor change

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === "production") {
     
 
     app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 } 
 
